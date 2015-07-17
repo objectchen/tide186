2b20462d0cda5985a9d5293e9b5cb89b7be02292
objectchen <objectchen@gmail.com>
2015-07-17 09:29:25 +0800

    loginAction

--- demo/src/main/java/com/demo/action/login/LoginAction.java
+++ demo/src/main/java/com/demo/action/login/LoginAction.java
@@ -1,6 +1,8 @@
 package com.demo.action.login;
 
 import javax.annotation.PostConstruct;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
@@ -11,6 +13,11 @@ public class LoginAction {
 
 	@RequestMapping("/haitao.do")
 	public String login(){
+		HttpServletRequest r=null;
+		r.getServerName();
+		r.getServerPort();
+		r.getServletPath();
+		HttpServletResponse p=null;
 		System.out.println("sssss");
 		return "tao";
 	}
